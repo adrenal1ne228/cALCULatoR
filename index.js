4e0a682dae9aa60fe769e25910d2c26ece40240d
adrenal1ne228 <149685357+adrenal1ne228@users.noreply.github.com>
2023-12-23 01:48:00 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+let num1, num2, result;
+
+function plus(){
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result=num1+num2;
+
+    document.getElementById('out').innerHTML = result;
+}
+
+function minus(){
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result=num1-num2;
+
+    document.getElementById('out').innerHTML = result;
+}
+
+function multiply(){
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result=num1*num2;
+
+    document.getElementById('out').innerHTML = result;
+}
+
+function division(){
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result=num1/num2;
+
+    document.getElementById('out').innerHTML = result;
+}
